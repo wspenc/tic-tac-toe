966955db64d0b89c624462c3fa10d469fab245da
Will Spencer <willspencer@me.com>
2022-11-22 14:48:28 -0700

    step 2

---
+++ src/Square.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Square = (props) => {
+    return (
+        <div className = "square">
+            {props.propsVar}
+        </div>
+    )
+}
+
+export default Square
+
+ 
